9114befe854d1277348399d963188b587e7da3d5
Durgasri <challadurgasri2000@gmail.com>
2025-09-30 23:42:33 +0530

    Just changed the cookie expiration

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -1,13 +1,11 @@
 export const sendToken = async (user, res, statusCode) => {
-  console.log(user)
   const token = user.getJWTToken();
-  console.log(token)
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: false, // 
+    secure: false, //
     sameSite: "lax",
   };
   res
