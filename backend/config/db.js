2bda7efdca216d60cce585628970d334245b84f0
Durgasri <challadurgasri2000@gmail.com>
2025-10-01 00:11:23 +0530

    mongo uri changed to keep standard practice

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const res = await mongoose.connect(process.env.mongoURI);
+    const res = await mongoose.connect(process.env.MONGO_URI);
     console.log(`mongodb connected with server ${res.connection.host}`)
   } catch (error) {
     console.log(error);
