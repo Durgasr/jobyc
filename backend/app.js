14fc0da6e7360a12f1e5cfbffb6e04c63f78f0e3
Durgasri <challadurgasri2000@gmail.com>
2025-09-30 23:39:59 +0530

    Add middleware to access files of uploads folder

--- backend/app.js
+++ backend/app.js
@@ -22,6 +22,7 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 app.use("/api/jobyc/user", userRoutes);
 app.use("/api/jobyc/jobs", jobsRouter);
