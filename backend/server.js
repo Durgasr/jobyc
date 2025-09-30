b49e0dd5d97cfb376a1cd3c1e1635a4e72cb91a0
Durgasri <challadurgasri2000@gmail.com>
2025-10-01 00:12:09 +0530

    add port as seperate variable

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
 
-app.listen(process.env.PORT, async (err) => {
+const port = process.env.PORT ||3700
+
+app.listen(port, async (err) => {
   if (err) {
     console.log(`server failed with error ${err}`);
   } else {
     await connectDB();
-    console.log(`server is running at http://localhost:${(process.env.PORT)}`);
+    console.log(`server is running at http://localhost:${port}`);
   }
 });
