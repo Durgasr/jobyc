b95b17db5b24be8373f182b70075d7bfd2b7629d
Durgasri <challadurgasri2000@gmail.com>
2025-12-03 23:12:38 +0530

    add navbar component and styles for it

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,18 +1,29 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../context/userContext";
 import styles from "./Navbar.module.css";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export const Navbar = () => {
   const { user, setUser } = useContext(UserContext);
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [userImg, setUserImg] = useState("./user.jpg");
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user && user.profileImage !== "" && user.profileImage !== "null" && user.profileImage != undefined) {
+      setUserImg(
+        `https://jobyc-4ad8ff06194c.herokuapp.com${user.profileImage}`
+      );
+    } else {
+      setUserImg("/user.jpg");
+    }
+  }, [user]);
 
   const handleLogout = async () => {
     try {
       await axios.post(
-        "https://jobyc-backend.onrender.com/api/jobyc/user/logout",
+        "https://jobyc-4ad8ff06194c.herokuapp.com/api/jobyc/user/logout",
         {},
         { withCredentials: true }
       );
@@ -24,14 +35,18 @@ export const Navbar = () => {
     }
   };
 
+
   return (
     <nav className={styles.navbar}>
-      <div className={styles.logo}>Jobyc</div>
+      <div className={styles.logoContainer}>
+        <img src="/jobyc.png" className={styles.jobycLogo} alt="logo" />
+        <p>Joby Careers</p>
+      </div>
 
       {user && (
         <div className={styles.profile}>
           <img
-            src="/user.jpg"
+            src={userImg}
             alt="Profile"
             className={styles.profilePic}
             onClick={() => setDropdownOpen(!dropdownOpen)}
@@ -43,7 +58,10 @@ export const Navbar = () => {
 
           {dropdownOpen && (
             <div className={styles.dropdown}>
-              <button>Edit Profile</button>
+              <button>
+                {" "}
+                <Link to="/profile-setup">Edit Profile</Link>
+              </button>
             </div>
           )}
         </div>
